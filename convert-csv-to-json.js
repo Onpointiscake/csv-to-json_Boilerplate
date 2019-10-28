792c9bfa5c6278934bfc09b036aafa6136110c5d
Javier Porlono <pauli44ef@gmail.com>
2019-10-28 15:40:30 +0100

    first commit

---
+++ convert-csv-to-json.js
@@ -0,0 +1,25 @@
+
+
+
+// desired output:
+
+/* arraydeEjemplo = [
+    {
+        id: 1,
+        first_name: Javier
+    },
+    {
+        id: 2,
+        first_name: Bunn
+    },
+    {
+        id: 3,
+        first_name: Mary
+    },
+    {
+        id: 4,
+        first_name: Oscar
+    },
+] 
+
+*/
